0e6feeba4d15f05aa6876bab0dc5b7bde3eb9ca3
Galib Mia <gm71bd@gmail.com>
2024-07-07 19:59:16 +0600

    Make responsive header, footer and hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='header-bg flex items-center justify-center gap-4'>
             <img className='w-20 h-24' src={logo} alt="" />
-            <h1 className='rancho-regular text-6xl text-white'>Espresso Emporium</h1>
+            <h1 className='rancho-regular text-4xl md:text-6xl text-white'>Espresso Emporium</h1>
         </div>
     );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,9 @@ import "./Hero.css"
 const Hero = () => {
     return (
         <div className='hero-bg flex '>
-            <div className='mt-96 w-4/5 mx-auto'>
+            <div className='mt-10 md:mt-96 w-4/5 mx-auto'>
             <div className='ms-custom-50'>
-                <h1 className='text-6xl rancho-regular text-white mb-4'>Would you like a Cup of Delicious Coffee?</h1>
+                <h1 className='text-4xl md:text-6xl rancho-regular text-white mb-4'>Would you like a Cup of Delicious Coffee?</h1>
                 <p className='text-white text-base raleway-regular'>It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
                 <button className='bg-[#E3B577] rancho-regular text-2xl px-6 py-2 rounded-sm mt-8 btn-custom'>Learn More</button>
             </div>
